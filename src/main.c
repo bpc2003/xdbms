d911f2491ffc3aa60588d01540df56b259891df1
bpc2003 <wpesfriendnva@gmail.com>
2025-03-10 18:00:06 -0400

    Add test.db

--- src/main.c
+++ src/main.c
@@ -1,9 +1,34 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "lib/mdb.h"
 
 int main(int argc, char **argv)
 {
-  printf("Hello, World!\n");
-  return 0;
+  if (argc != 2)
+    exit(1);
+  char *filename = argv[1];
+  struct keytablist *list = readdb(filename);
+
+  for (int i = 0; i < list[0].len; ++i) {
+    int *indexes = getkeys(list, i);
+    for (int j = 0; indexes[j]; ++j) {
+      printf("%s: ", list[i].tab[indexes[j]].key);
+      switch (list[i].tab[indexes[j]].flag) {
+        case 1:
+          printf("%.2lf\n", list[i].tab[indexes[j]].v.num);
+          break;
+        case 2:
+          printf("%d\n", list[i].tab[indexes[j]].v.b);
+          break;
+        case 3:
+          printf("%s\n", list[i].tab[indexes[j]].v.str);
+          break;
+      }
+      delkey(list, i, list[i].tab[indexes[j]].key);
+    }
+    free(indexes);
+  }
+  free(list);
+  exit(0);
 }
